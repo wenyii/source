dd3c12a8bc84a34092ed12907e6e44d9b760e1e0
Leon <jiangxilee@gmail.com>
2017-05-04 17:55:41 +0800

    微信支付流程优化

--- kake-frontend/js/main.js
+++ kake-frontend/js/main.js
@@ -1099,27 +1099,4 @@ app.controller('generic', ['$scope', '$q', '$timeout', 'genericService', 'generi
             }
         });
     };
-
-    // 微信吊起支付
-    $scope.wxPayment = function (data) {
-        function onBridgeReady() {
-            WeixinJSBridge.invoke('getBrandWCPayRequest', data, function (response) {
-                    if (response.err_msg === 'get_brand_wcpay_request:ok') {
-
-                    }
-                }
-            );
-        }
-
-        if (typeof WeixinJSBridge === 'undefined') {
-            if (document.addEventListener) {
-                document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-            } else if (document.attachEvent) {
-                document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
-                document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-            }
-        } else {
-            onBridgeReady();
-        }
-    };
 }]);
