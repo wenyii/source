22ea333b30dd319e3e9ec75e1e2194a42b7cce39
Leon <jiangxilee@gmail.com>
2017-07-07 18:39:57 +0800

    修复继承公共ctl

--- kake/frontend/js/distribution/index.js
+++ kake/frontend/js/distribution/index.js
@@ -1,9 +1,16 @@
 /**
  * 控制器 - 分销
  */
+<<<<<<< Updated upstream
 app.controller('distribution', function ($scope, $controller, $timeout) {
+=======
+app.controller('distribution', ['$scope', '$controller', '$timeout', function ($scope, $controller, $timeout) {
+
+	$controller('generic', {$scope: $scope});
+	
+>>>>>>> Stashed changes
 	$scope.isShowAni = true;
 	$timeout(function(){
 		$scope.isShowAni = false ;
 	},5000)
-});
+}]);
